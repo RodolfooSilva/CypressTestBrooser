4455c261f2e2b9deff11c3f156d3137ba5c81d7b
Rodolfo Silva <dolfo.3@hotmail.com>
2022-06-10 18:57:50 -0300

    readme criado

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,49 @@
+/// <reference types= "cypress"/>
+
+describe('Meu Primeiro Test Cypress', () => {
+
+    it ('Test 1', () => {
+        cy.visit ('https://www.google.com/')
+     
+     })
+
+     it ('Test 1', () => {
+        cy.visit ('https://www.amazon.com/')
+     
+     })
+
+     it ('Test 1', () => {
+        cy.visit ('https://www.itau.com/')
+     
+     })
+     it ('Test 1', () => {
+        cy.visit ('https://www.americanas.com/')
+     
+     })
+
+     
+})
+
+describe('Meu Primeiro Test Cypress', () => {
+
+    it ('Test 1', () => {
+        cy.visit ('https://www.google.com/')
+     
+     })
+
+     it ('Test 1', () => {
+        cy.visit ('https://www.amazon.com/')
+     
+     })
+
+     it ('Test 1', () => {
+        cy.visit ('https://www.itau.com/')
+     
+     })
+     it ('Test 1', () => {
+        cy.visit ('https://www.americanas.com/')
+     
+     })
+
+     
+})
